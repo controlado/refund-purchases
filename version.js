413faa37a68b94319ee9bf58995f0d38bd31f866
balaclava <yangabrielogy@gmail.com>
2023-06-01 03:56:16 -0300

    test: testando função de update

---
+++ version.js
@@ -0,0 +1,13 @@
+/**
+ * @author
+ * Nome: Yan Gabriel    
+ * Discord: Balaclava#1912 (854886148455399436)    
+ * GitHub: https://github.com/controlado
+ */
+
+const plugin = {
+    "name": "Refund Last Purchase",
+    "url": "https://github.com/controlado/refund-last-purchase",
+    "version": "1.0.1",
+}
+export default plugin
