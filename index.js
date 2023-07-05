d23c70937be0d4779c8213f4ddfb4cf8b16a1f9d
balaclava <yangabrielogy@gmail.com>
2023-07-04 21:13:19 -0300

    refactor: código formatado
    tipagem
    documentação
    ponto e vírgulas

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { sleep, addRoutines } from "../controladoUtils"
-import { sendNotification, Store } from "./requests"
+import { addRoutines, sleep } from "../controladoUtils";
+import { sendNotification, Store } from "./requests";
 
 /**
  * @author balaclava
@@ -9,73 +9,75 @@ import { sendNotification, Store } from "./requests"
  */
 
 const buttonConfig = {
-    id: "refund-last-purchase",
-    defaultMode: "refund",
-    text: {
-        default: "Refund",
-        refund: "Refund Last Champion",
-        buy: "Buy Last Refunded Champion",
-    }
-}
+  id: "refund-last-purchase",
+  defaultMode: "refund",
+  text: {
+    default: "Refund",
+    refund: "Refund Last Champion",
+    buy: "Buy Last Refunded Champion",
+  },
+};
 const plugin = {
-    "name": "Refund Last Purchase",
-    "url": "https://github.com/controlado/refund-last-purchase",
-    "version": "1.0.1",
-}
+  name: "Refund Last Purchase",
+  url: "https://github.com/controlado/refund-last-purchase",
+  version: "1.0.1",
+};
 
 function onMutation() {
-    const championSelectButtons = document.querySelector(".bottom-right-buttons")
-    if (!championSelectButtons || document.getElementById(buttonConfig.id)) { return }
+  const championSelectButtons = document.querySelector(".bottom-right-buttons");
+  if (!championSelectButtons || document.getElementById(buttonConfig.id)) { return; }
 
-    const store = new Store()
+  const store = new Store();
 
-    const buyChampionButton = document.createElement("lol-uikit-flat-button")
-    buyChampionButton.setAttribute("style", "right: 19px; bottom: 57px; align-items: flex-end; position: absolute; display: flex;")
-    buyChampionButton.textContent = buttonConfig.text.default
-    buyChampionButton.mode = buttonConfig.defaultMode
-    buyChampionButton.id = buttonConfig.id
+  const buyChampionButton = document.createElement("lol-uikit-flat-button");
+  buyChampionButton.setAttribute("style", "right: 19px; bottom: 57px; align-items: flex-end; position: absolute; display: flex;");
+  buyChampionButton.textContent = buttonConfig.text.default;
+  buyChampionButton.mode = buttonConfig.defaultMode;
+  buyChampionButton.id = buttonConfig.id;
 
-    buyChampionButton.onclick = async () => {
-        buyChampionButton.setAttribute("disabled", "true")
-        const purchaseHistory = await store.getPurchaseHistory()
-        try {
-            if (buyChampionButton.mode === "refund") {
-                const response = await store.refundLastChampion(purchaseHistory)
-                await sendNotification(`Refund: ${response.statusText} (${response.status})`)
-            } else {
-                const item = purchaseHistory.find(item => item.currencyType === "IP" && item.inventoryType === "CHAMPION" && item.refundabilityMessage === "ALREADY_REFUNDED")
-                const items = [
-                    {
-                        inventoryType: "CHAMPION",
-                        ipCost: item.amountSpent,
-                        itemId: item.itemId,
-                        quantity: 1,
-
-                    },
-                ]
-                const requestBody = { "accountId": store.summoner.accountId, "items": items }
-                const response = await store.request("POST", "/storefront/v3/purchase", requestBody)
-                await sendNotification(`Purchase: ${response.statusText} (${response.status})`)
-            }
-        }
-        catch (error) { await sendNotification(error.message) }
-        finally { buyChampionButton.removeAttribute("disabled") }
+  buyChampionButton.onclick = async () => {
+    buyChampionButton.setAttribute("disabled", "true");
+    const purchaseHistory = await store.getPurchaseHistory();
+    try {
+      if (buyChampionButton.mode === "refund") {
+        const response = await store.refundLastChampion(purchaseHistory);
+        await sendNotification(`Refund: ${response.statusText} (${response.status})`);
+      }
+      else {
+        const item = purchaseHistory.find(item => item.currencyType === "IP" && item.inventoryType === "CHAMPION" && item.refundabilityMessage === "ALREADY_REFUNDED");
+        const items = [
+          {
+            inventoryType: "CHAMPION",
+            ipCost: item.amountSpent,
+            itemId: item.itemId,
+            quantity: 1,
+          },
+        ];
+        const requestBody = { "accountId": store.summoner.accountId, "items": items };
+        const response = await store.request("POST", "/storefront/v3/purchase", requestBody);
+        await sendNotification(`Purchase: ${response.statusText} (${response.status})`);
+      }
     }
-    buyChampionButton.oncontextmenu = () => {
-        buyChampionButton.mode = buyChampionButton.mode === "refund" ? "buy" : "refund"
-        buyChampionButton.textContent = buttonConfig.text[buyChampionButton.mode]
-    }
-    buyChampionButton.onmouseenter = () => { buyChampionButton.textContent = buttonConfig.text[buyChampionButton.mode] }
-    buyChampionButton.onmouseleave = () => { buyChampionButton.textContent = buttonConfig.text.default }
+    catch (error) { await sendNotification(error.message); }
+    finally { buyChampionButton.removeAttribute("disabled"); }
+  };
+  buyChampionButton.oncontextmenu = () => {
+    buyChampionButton.mode = buyChampionButton.mode === "refund" ? "buy" : "refund";
+    buyChampionButton.textContent = buttonConfig.text[buyChampionButton.mode];
+  };
+  buyChampionButton.onmouseenter = () => buyChampionButton.textContent = buttonConfig.text[buyChampionButton.mode];
+  buyChampionButton.onmouseleave = () => buyChampionButton.textContent = buttonConfig.text.default;
 
-    sleep(1000).then(() => { // @teisseire117 - league-loader-plugins/dodge_button
-        const dodgeContainer = document.getElementsByClassName("dodge-button-container")
-        if (dodgeContainer.length > 0) { sendNotification("Identified dodge-button!"); buyChampionButton.style.bottom = "96px" }
-        championSelectButtons.parentNode.insertBefore(buyChampionButton, championSelectButtons)
-    })
+  sleep(1000).then(() => { // @teisseire117 - league-loader-plugins/dodge_button
+    if (document.getElementsByClassName("dodge-button-container").length > 0) {
+      sendNotification("Identified dodge-button!");
+      buyChampionButton.style.bottom = "96px";
+    }
+    championSelectButtons.parentNode.insertBefore(buyChampionButton, championSelectButtons);
+  });
 }
 
 window.addEventListener("load", () => {
-    console.debug(`${plugin.name}: Report bugs to Balaclava#1912`)
-    addRoutines(onMutation)
-})
+  console.debug(`${plugin.name}: Report bugs to Balaclava#1912`);
+  addRoutines(onMutation);
+});
--- requests.js
+++ requests.js
@@ -1,4 +1,4 @@
-import { StoreBase, credentials } from "../controladoUtils"
+import { credentials, request, StoreBase } from "../controladoUtils";
 
 /**
  * @author balaclava
@@ -7,73 +7,64 @@ import { StoreBase, credentials } from "../controladoUtils"
  * @description Play with a champion for free! 🐧
  */
 
+/**
+ * Envia uma notificação no chat da seleção de campeões.
+ *
+ * @async
+ * @function
+ * @param {string} notification - Notificação que vai ser enviada.
+ * @return {Promise<Response>} Resposta da requisição.
+ */
 export async function sendNotification(notification) {
-    const { participants } = await getChampionSelectParticipants()
-    const { cid } = participants[0]
+  const { participants } = await getChampionSelectParticipants();
+  const { cid } = participants[0];
 
-    const requestParams = {
-        method: "POST",
-        headers: {
-            "accept": "application/json",
-            "content-type": "application/json"
-        },
-        body: JSON.stringify(
-            {
-                body: notification,
-                type: "celebration"
-            }
-        )
-    }
-    const endpoint = `/lol-chat/v1/conversations/${cid}/messages`
-    const response = await fetch(endpoint, requestParams)
-    return await response.json()
+  const requestBody = { body: notification, type: "celebration" };
+  const endpoint = `/lol-chat/v1/conversations/${cid}/messages`;
+  const response = await request("POST", endpoint, { body: requestBody });
+  return await response.json();
 }
 
-export async function getChampionSelectParticipants() {
-    const authToken = btoa(`riot:${credentials.auth}`)
-    const requestParams = {
-        method: "GET",
-        headers: {
-            Authorization: `Basic ${authToken}`
-        }
-    }
-    const url = `https://127.0.0.1:${credentials.port}/chat/v5/participants/champ-select`
-    const response = await fetch(url, requestParams)
-    return await response.json()
+async function getChampionSelectParticipants() {
+  const authToken = btoa(`riot:${credentials.auth}`);
+  const requestHeaders = { Authorization: `Basic ${authToken}` };
+  const url = `https://127.0.0.1:${credentials.port}/chat/v5/participants/champ-select`;
+  const response = await request("GET", url, { headers: requestHeaders });
+  return await response.json();
 }
 
 export class Store extends StoreBase {
-    /**
-     * Tenta reembolsar o último campeão.
-     * 
-     * @async
-     * @function
-     * @summary Deve ser chamada após a conclusão do {@link auth}.
-     * @param {JSON[]} purchaseHistory - Obtido através do {@link getPurchaseHistory}.
-     * @return {Promise<Response>} Resposta da tentativa de requisição enviada.
-     */
-    async refundLastChampion(purchaseHistory) {
-        const champions = purchaseHistory.filter(purchase => purchase.inventoryType === "CHAMPION")
-        if (champions.length === 0) { return }
-
-        const body = {
-            accountId: this.summoner.accountId,
-            transactionId: champions[0]["transactionId"],
-            inventoryType: "CHAMPION"
-        }
-        return await this.request("POST", "/storefront/v3/refund", body)
+  /**
+   * Tenta reembolsar o último campeão.
+   *
+   * @async
+   * @function
+   * @summary Deve ser chamada após a conclusão do {@link auth}.
+   * @param {Object[]} purchaseHistory - Obtido através do {@link getPurchaseHistory}.
+   * @return {Promise<Response>} Resposta da tentativa de requisição enviada.
+   */
+  async refundLastChampion(purchaseHistory) {
+    const champions = purchaseHistory.filter(purchase => purchase.inventoryType === "CHAMPION");
+    if (champions.length) {
+      const body = {
+        inventoryType: "CHAMPION",
+        transactionId: champions[0]["transactionId"],
+        accountId: this.summoner.accountId,
+      };
+      return await this.request("POST", "/storefront/v3/refund", body);
     }
+  }
 
-    /**
-     * Retorna o histórico de compras do jogador autenticado.
-     * 
-     * @async
-     * @function
-     * @summary Deve ser chamada após a conclusão do {@link auth}.
-     * @return {Promise<JSON[]>} Histórico de transações do jogador.
-     */
-    async getPurchaseHistory() {
-        const response = await this.request("GET", "/storefront/v3/history/purchase")
-        return response.data.transactions
-    }
+  /**
+   * Retorna o histórico de compras do jogador autenticado.
+   *
+   * @async
+   * @function
+   * @summary Deve ser chamada após a conclusão do {@link auth}.
+   * @return {Promise<Object[]>} Histórico de transações do jogador.
+   */
+  async getPurchaseHistory() {
+    const response = await this.request("GET", "/storefront/v3/history/purchase");
+    return response.data.transactions;
+  }
 }
