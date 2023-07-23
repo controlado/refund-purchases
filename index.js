780079bb7aedf393d73e172d540ef3c512679541
Yan Gabriel <yangabrielogy@gmail.com>
2023-07-23 00:46:21 -0300

    fix: index adicionado

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * @author balaclava
+ * @name refund-purchases
+ * @link https://github.com/controlado/refund-purchases
+ * @description Play with a champion or skin for free! 🐧
+ */
+
+import "https://cdn.skypack.dev/refund-purchases@latest?min";
